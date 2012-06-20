312253254053e97e6ffc6b517afd4ea311ed391f
Michael Ducharme <mducharme@gobcn.ca>
2012-06-20 11:35:16 -0500

    Added Trango Image

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.BCN.Trango"
-VERSION = "1.45"
+VERSION = "1.47"
 AUTHOR = "Michael Ducharme"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.BCN']
